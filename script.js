67944812b3d5d7c2aed84c88f1d46c08d08785b6
1475093 <41354503+1475093@users.noreply.github.com>
2023-06-28 21:46:26 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -32,8 +32,9 @@ function playSound(e) {
     let key = e.keyCode || e.target.dataset.key;
     div = document.querySelector('div [data-key="' + key + '"]');
     if (div) {
+        // 黄色表示
         div.classList.toggle("clickedkey")
-        // div要素のテキスト(音名)を代入する
+        // div要素の音名を代入する
         synth.triggerAttackRelease(div.textContent, '16n');
         // 状態をfalseにして、連続的な発音を防止する
         state = false;
@@ -41,36 +42,47 @@ function playSound(e) {
 }
 
 function offSound(e) {
+    // 黄色表示を解除
     let key = e.keyCode || e.target.dataset.key;
     div = document.querySelector('div [data-key="' + key + '"]');
     if (div) {
         div.classList.toggle("clickedkey")
     }
+
     state = true;  // 再度、発音できるようにtrueへ戻す
 }
 
 
-//楽譜を食わせてボタンを押すと演奏する機能を実装
+// //楽譜を食わせてボタンを押すと演奏する機能を実装
 
-// メロディの音符を配列で保持
-wa.playSilent(); //Chromeデバッグ用の無音再生
-let melodyList = [
-    'C4', 'D4', 'E4', 'F4',
-    'G4', ['A4', 'B4'], 'C5'
-];
-let synth2 = new Tone.Synth().toDestination();
+// const melodyList = [
+//     ['D4', 'D4', 'D4'], 'D4', 'A#3', 'C4', 'D4', ['C4', 'D4']
+// ];
+
+// //ボタンが押されたらplayMelodyを実行
+// document.getElementById("button").addEventListener("click", playMelody);
+
+// function playMelody() {
+//     // wa.playSilent(); //Chromeデバッグ用の無音再生
+
+//     // let synth2 = new Tone.Synth().toDestination();
+
+//     // メロディをシーケンス制御する
+//     let melody = new Tone.Sequence(setPlay, melodyList).start();
+
+//     melody.loop = 1;  // ループを1回に設定
+//     Tone.Transport.bpm.value = 90;  // BPMを90に設定
+//     Tone.Transport.start('1m');  // 1小節分の時間をおいてから発音
+
+//     function setPlay(time, note) {
+//         // 8分音符でメロディを発音
+//         //   wa.playSilent(); //Chromeデバッグ用の無音再生
+//         synth.triggerAttackRelease(note, '8n', time);
+//     };
+
+
+// };
 
 
 
-// メロディをシーケンス制御する
-let melody = new Tone.Sequence(setPlay, melodyList).start();
 
-melody.loop = 1;  // ループを1回に設定
-Tone.Transport.bpm.value = 90;  // BPMを90に設定
-Tone.Transport.start('1m');  // 1小節分の時間をおいてから発音
-
-function setPlay(time, note) {
-    // 8分音符でメロディを発音
-    //   wa.playSilent(); //Chromeデバッグ用の無音再生
-    synth.triggerAttackRelease(note, '8n', time);
-}
