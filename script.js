e8fe26b5420eb4e7132da950c7c8383c1f9f9fcd
1475093 <41354503+1475093@users.noreply.github.com>
2023-06-30 20:15:01 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 // 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-// npm i Tone;
-// import * as Tone from 'tone';
+//Chromeデバッグ用
+wa.playSilent();
+let context = new AudioContext();
 
-wa.playSilent(); //Chromeデバッグ用の無音再生
-
-// Tone.Synth()の使用準備
+// 音源の使用準備
 const synth = new Tone.Synth().toDestination();
 
-// test
+// test play
 // synth.triggerAttckRelease("C4", "4n")
 
-let state = true;  // 入力監視初期化
+
+
+
+////////////////// ピアノアプリ ///////////////////////////
+// 発音許可初期化
+let state = true;
 
 // ボタンを押したときに継続発音するようにする
-// 「マウス」か「キーボード」を押した時のイベント処理
 window.addEventListener('keydown', playSound);
 window.addEventListener('mousedown', playSound);
 
-// 「マウス」か「キーボード」を離した時のイベント処理
 window.addEventListener('keyup', offSound);
 window.addEventListener('mouseup', offSound);
 
 
 function playSound(e) {
-    if (!state) return;  // falseなら処理を実行しない
-
+    if (!state) return;  // 発音許可確認
     wa.playSilent(); //Chromeデバッグ用の無音再生
 
     let key = e.keyCode || e.target.dataset.key;
     div = document.querySelector('div [data-key="' + key + '"]');
     if (div) {
-        // 黄色表示
+
+        // 鍵盤の黄色表示
         div.classList.toggle("clickedkey")
+
         // div要素の音名を代入する
         synth.triggerAttackRelease(div.textContent, '16n');
+
         // 状態をfalseにして、連続的な発音を防止する
         state = false;
     }
@@ -49,109 +53,8 @@ function offSound(e) {
         div.classList.toggle("clickedkey")
     }
 
-    state = true;  // 再度、発音できるようにtrueへ戻す
+    state = true;  // 再度、発音許可
 }
 
-
-// //楽譜を食わせてボタンを押すと演奏する機能を実装
-
-// const melodyList = [
-//     ['D4', 'D4', 'D4'], 'D4', 'A#3', 'C4', 'D4', ['C4', 'D4']
-// ];
-
-// //ボタンが押されたらplayMelodyを実行
-// document.getElementById("button").addEventListener("click", playMelody);
-
-// function playMelody() {
-//     // wa.playSilent(); //Chromeデバッグ用の無音再生
-
-//     // let synth2 = new Tone.Synth().toDestination();
-
-//     // メロディをシーケンス制御する
-//     let melody = new Tone.Sequence(setPlay, melodyList).start();
-
-//     melody.loop = 1;  // ループを1回に設定
-//     Tone.Transport.bpm.value = 90;  // BPMを90に設定
-//     Tone.Transport.start('1m');  // 1小節分の時間をおいてから発音
-
-//     function setPlay(time, note) {
-//         // 8分音符でメロディを発音
-//         //   wa.playSilent(); //Chromeデバッグ用の無音再生
-//         synth.triggerAttackRelease(note, '8n', time);
-//     };
-// };
-
-const btn = document.getElementById("btn");
-const play = document.getElementById("play");
-const stop = document.getElementById("stop");
-
-//STOPボタン非表示
-stop.style.display = "none"; 
-
-//メロディ
-const melodyLine = [
-  ['0:0:0','c5'],
-  ['0:3:0','G4'],
-  ['1:2:0','E4'],
-  ['2:0:0','A4'],
-  ['2:1.5:0','B4'],
-  ['2:2.8:0','A4'], 
-  ['3:0.2:0','G#4'],
-  ['3:1.6:0','A#4'], 
-  ['3:2.8:0','G#4'],
-  ['4:0:0','G4'], 
-  ['4:0.75:0','F#4'],
-  ['4:1.25:0','G4']
-];
-
-//再生ボタン
-play.addEventListener("click", function () { 
-
-//ボタン表示切替
-play.style.display = "none"; 
-stop.style.display = "block"; 
-
-wa.playSilent(); //Chromeデバッグ用の無音再生
-  
-//音源
-const synth = new Tone.AMSynth().toMaster(); 
- 
-//8分音符で再生
-function setPlay(time, note) { synth.triggerAttackRelease(note,'8n', time);
-}
-
-//メロディをセット  
-const melody = new Tone.Part(setPlay, melodyLine); 
-
-//メロディ再生
-melody.start();
-
-//ループ（デフォルトでfalse）
-melody.loop = true;
-melody.loopStart = "0";
-melody.loopEnd = "16";
-
-//テンポ
-Tone.Transport.bpm.value = 400;
-  
-//再生実行
-Tone.Transport.start(); 
-
-}, false);
-
-
-//停止ボタン
-stop.addEventListener("click", function () { 
- 
-//ボタン表示切替
-stop.style.display = "none"; 
-play.style.display = "block"; 
-  
-//停止実行
-Tone.Transport.stop();
-//イベントクリア
-Tone.Transport.cancel();
-
-}, false);
 
 
